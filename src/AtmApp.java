cf8c11a58f58618c8bd41ad4f3fe1813d69e20fa
GulsumStc <121710851+GulsumStc@users.noreply.github.com>
2023-02-22 21:32:02 +0300

    v2

--- src/AtmApp.java
+++ src/AtmApp.java
@@ -3,6 +3,8 @@ public class AtmApp {
     public static void main(String[] args) {
 
         System.out.println("Bu bit ATM projesidir");
+        //version 2
+        System.out.println("Java is easy");
 
 
     }
