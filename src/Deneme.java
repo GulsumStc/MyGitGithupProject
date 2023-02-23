1ba4cef381cfbcf86e281700ada0ecd6115fe14d
GulsumStc <121710851+GulsumStc@users.noreply.github.com>
2023-02-23 20:26:33 +0300

    d3

--- src/Deneme.java
+++ src/Deneme.java
@@ -5,6 +5,8 @@ public class Deneme {
         System.out.println("bi bir deneme projesidir");
         //deneme2
         System.out.println("im a super developer");
+        //deneme3
+        System.out.println("i willlllll be a super developer");
 
 
     }
