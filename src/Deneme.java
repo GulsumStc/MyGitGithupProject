2e51838f250309ac517d4a1bcf33cde8ce46b1ea
GulsumStc <121710851+GulsumStc@users.noreply.github.com>
2023-02-23 20:26:12 +0300

    d2

--- src/Deneme.java
+++ src/Deneme.java
@@ -3,6 +3,8 @@ public class Deneme {
     public static void main(String[] args) {
 
         System.out.println("bi bir deneme projesidir");
+        //deneme2
+        System.out.println("im a super developer");
 
 
     }
